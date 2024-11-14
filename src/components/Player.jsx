d00badfb5da0eb6a0ffcd1b4abaead232d7ea18b
Mesud-Ahmed <mesudahmed94@gmail.com>
2024-11-14 09:51:46 +0300

    feat: add Gameboard component to display the grids

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,26 @@
-export default function Player({name,symbol}) {
+import { useState } from "react"
+export default function Player({ initialName, symbol }) {
+    const [playerName,setPlayerName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+    const handleClick = () => {
+        setIsEditing((edting) => !edting)
+    }
+    const handleChange = (e) =>{
+        setPlayerName(e.target.value)
+    }
+    let editedName = <span className="player-name">{playerName}</span>
+    if(isEditing){
+        editedName = <input type="text" required Value={playerName} onChange={handleChange}></input>
+    }
     return (
+
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+               {editedName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            
+            <button onClick={handleClick}>{isEditing ? 'Save': 'Edit'}</button>
         </li>
     )
 }
